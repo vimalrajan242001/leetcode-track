93e1bdedfe5546abeb72287844ee20de24fc4aae
Vimal Rajan G <64751374+vimalrajan242001@users.noreply.github.com>
2025-03-25 23:39:42 +0530

    Time: 3 ms (6.16%) | Memory: 12.5 MB (68.52%) - LeetSync

--- cpp/861-flipping-an-image/flipping-an-image.cpp
+++ cpp/861-flipping-an-image/flipping-an-image.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
-        auto cp = image;
         int n = image.size();
-        for(int i=0; i<n;i++){
-            int m = n%2 == 0 ? n/2 : (n/2) + 1;
-            for(int j= 0; j<m;j++){
-                int temp = cp[i][j];
-                cp[i][j] = cp[i][n-j-1] == 0 ? 1 : 0;
-                cp[i][n-j-1] = temp == 0 ? 1 : 0; 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < (n + 1) / 2; j++) {
+                // Swap and invert simultaneously
+                int temp = image[i][j];
+                image[i][j] = image[i][n - j - 1] == 0 ? 1 : 0;
+                image[i][n - j - 1] = temp == 0 ? 1 : 0;
             }
         }
-
-        return cp;
+        return image;
     }
 };
